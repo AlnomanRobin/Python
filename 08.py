c93912f4f3c1b09e8503d64169b9e3967ff0b238
Alnoman Robin <robinalnoman@gmail.com>
2025-04-07 22:43:39 +0600

    Add files via upload

---
+++ 08.py
@@ -0,0 +1,10 @@
+#Taking input from user & printing it
+name = input("Name : ")
+print(name)
+#input("Age : ")
+
+price=int (input("Price : "))
+print("The price is : ",price)
+
+mrp=float(input("MRP : "))
+print(mrp)

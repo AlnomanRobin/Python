04ceacdde5178afb5f4fcd78c88b5b2633987f4e
Alnoman Robin <robinalnoman@gmail.com>
2025-04-08 18:40:48 +0600

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,10 @@
+name=input("name : ")
+age=int(input("age : "))
+price=float(input("Price : "))
+print("My name is",name,"and my age is",age,"years old. My book price is",price)
+
+
+"""
+name=(input( ))
+print(name)
+"""
---
+++ 10.py
@@ -0,0 +1,10 @@
+#Traffic Light Code
+light=input("light : ")
+if(light=="red"):
+    print("Stop")
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("Go")
+else:
+    print("light is broken!")

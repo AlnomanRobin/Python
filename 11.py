5a1e39e5bb6398c480a7b4a69a0751829d33fd5b
Alnoman Robin <robinalnoman@gmail.com>
2025-04-12 01:12:09 +0600

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,10 @@
+#Grades of students
+marks=int(input("marks : "))# Convert input to an integer
+if(marks >= 90):
+    print("A")
+elif(marks >= 80 and marks < 90):
+    print("B")
+elif(marks >= 70 and marks < 80):
+    print("C")
+else:
+    print("D")

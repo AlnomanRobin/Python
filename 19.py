b9b6bd23529a57031685c7772637e81b78ad5d80
Alnoman Robin <robinalnoman@gmail.com>
2025-04-19 01:44:23 +0600

    Type Conversion

---
+++ 19.py
@@ -0,0 +1,15 @@
+# Type Conversion
+a=2
+b=4.25
+sum=a+b #2.0+4.25=>6.25
+print(sum)
+
+x,y=1,"2"# y="2" is a integer value
+z=int(y)# y=int("2")
+SUM=x+z
+print(SUM)
+print(type(x))
+
+p=3.1416
+p=str(a)# str=>string
+print(type(p))

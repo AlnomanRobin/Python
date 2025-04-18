aa65e20af6369e152e14f574badb7376bfed15c8
Alnoman Robin <robinalnoman@gmail.com>
2025-04-18 13:34:02 +0600

    Logical Operators(AND, OR, NOT)-Boolean

---
+++ 18.py
@@ -0,0 +1,20 @@
+#Logical Operators(and, or, not)-Boolean
+
+# NOT gate
+print(not False)
+print(not False)
+a=50
+b=30
+print(not (a>b)) #a>b is True.But not True=False
+print(not(b>a)) #not False=True
+
+# AND gate
+val1=True
+val2=True #True=1
+val3=False #False=0
+print("And gate is :",val1 and val2)
+print("And gate is : ",val2 and val3)
+
+# OR gate
+print("OR gate is : ",val2 or val3)
+print("OR gate is : ",(a==b) or (a>b))

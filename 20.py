1adffb4efc1d875dae75b7ab347678721e476cdb
Alnoman Robin <robinalnoman@gmail.com>
2025-05-08 00:17:46 +0600

    Type Conversion_2

---
+++ 20.py
@@ -0,0 +1,22 @@
+# Type Conversion
+a=5
+b="Al noman Robin"
+c=7.69
+d=100.984
+e="@"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+A = str("6")
+print(A)  # Output: "6" (as string)
+
+# To make B = int(Robin) work, we need to define Robin first
+Robin = "42"  # Example: Defining Robin as a numeric string
+B = int(Robin)
+print(B)  # Output: 42 (as integer)
+
+C = float(19)
+print(C)  # Output: 19.0 (as float)

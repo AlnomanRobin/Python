80c8fea1d5aa61873f3f81dc6c4e9bed834c80be
Alnoman Robin <robinalnoman@gmail.com>
2025-05-31 20:22:14 +0600

    #Python Numbers

---
+++ 21.py
@@ -0,0 +1,33 @@
+#Python Numbers
+x=1 #int
+y=2.8 #float
+z=1j #complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+a=1
+b=763243756889
+c=-523113
+print(type(a))
+print(type(b))
+print(type(c))
+
+d=1.32
+e=-64.59
+f=45786.7856543454
+print(type(d))
+print(type(e))
+print(type(f))
+
+g=35e3
+h=12E4
+i=-87.7e100
+print(type(g))
+print(type(h))
+print(type(i))
+
+j=3+5j
+k=5j # -5j
+print(type(j))
+print(type(k))

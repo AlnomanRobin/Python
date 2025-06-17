e1f5e3590a2568cc6a7e0d42e119bf284837d69e
Alnoman Robin <robinalnoman@gmail.com>
2025-06-17 22:44:40 +0600

    Python Strings

---
+++ 24.py
@@ -0,0 +1,19 @@
+#Python Strings
+print("Hello!")
+print('hello')
+
+print("It's alright")
+print("He is called 'Al Noman'") #-------
+print('He is called"Robin"') #--------
+
+A="HI!"
+print(A)
+
+C='''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sad do eiusmod'''
+print(C)
+
+B="""Gorem ipsum dolor sit amet, consectetur adipiscing elit, sad do Eiusmod"""
+print(B)
+
+a="Hello, World!"
+print(a[1])

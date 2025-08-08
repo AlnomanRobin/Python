d2b55f7974d7326f1d018cd4e80cc0a8cddbc9aa
Alnoman Robin <robinalnoman@gmail.com>
2025-08-08 19:48:02 +0600

    Sclicing Strings

---
+++ 27.py
@@ -0,0 +1,16 @@
+# Sclicing Strings
+a="HelloAL, World!" # H=0,e=1,l=2
+print(a[2:5]) # llo (2,3,4)
+
+b="Hello, World!"
+print(b[:5]) # 0,1,2,3,4 
+
+# Slice To the End
+c="Hello, World!"
+print(c[2:]) # 0,1,2
+
+d="Hello, World!"
+print(d[-5:-2])
+
+""" From: "o" in "World!" (position -5)
+To, but not included: "d" in "World!" (position -2) """

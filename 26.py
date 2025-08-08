edac8344f541574f1011985350a36555725dc224
Alnoman Robin <robinalnoman@gmail.com>
2025-08-08 19:44:00 +0600

    Check String

---
+++ 26.py
@@ -0,0 +1,15 @@
+# Check String
+txt1="the best things in life are free!"
+print("free" in txt1)
+
+txt2="the best things in life are free!"
+if "best" in txt2:
+    print("Yes, 'best' in present.\n")
+
+#Check if NOT
+txt3="the best things in life are free!"
+print("six" not in txt3)
+
+txt4="the best things in life are free!"
+if "expensive" not in txt4:
+    print("No,'expensive' is NOT presentr.\n")
